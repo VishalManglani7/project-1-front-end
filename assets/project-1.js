b2d49e4be7a96758f884cf6445b33551fe0e8e4f
malloryklar0516 <malloryklar@Mallorys-Air.cable.rcn.com>
2023-08-31 21:25:23 -0400

    built out js and added ajax call

--- assets/project-1.js
+++ assets/project-1.js
@@ -9,11 +9,29 @@
 //Upon clicking red X when movie is on the side bar, movie name is removed.
 
 function startApp(){
-    var requestURL = 'http://www.omdbapi.com/?i=tt3896198&apikey=5ee7c193' //title, rating, genre, runtime
-    fetch(requestURL)
+    var requestURL = 'http://www.omdbapi.com/?t=barbie&apikey=5ee7c193'; //title, rating, genre, runtime
+    $.ajax({
+        url: requestURL,
+        method: "GET"
+    })
         .then(function(response){
-            return response.json();
-        .then(function(){
-            return response.json();
-        }
-}}
+            console.log(response);
+            $('.title').text(response.Title);
+            $('.rating').text("Rated: "+ response.Rated);
+            $('.runtime').text("Runtime: "+ response.Runtime);
+            $('.genre').text("Genre: "+ response.Genre);
+            var seeList = JSON.parse(localStorage.getItem('seeList'))||[];
+            var title = response.Title;
+            $('.title').on('click', function(){
+                localStorage.setItem('title', title);
+                seeList.unshift(title);
+                localStorage.setItem('seeList',JSON.stringify(seeList));
+                $('.movie-list').append("<li>"+localStorage.getItem('title')+"</li>");
+    
+           })
+
+
+ })
+        
+}
+startApp();
