36042083ea1bcb409322e9bdc2b763c3eed87d07
malloryklar0516 <malloryklar@Mallorys-Air.cable.rcn.com>
2023-09-06 18:07:06 -0400

    made styling changes and added Oppenheimer

--- assets/project-1.js
+++ assets/project-1.js
@@ -9,29 +9,57 @@
 //Upon clicking red X when movie is on the side bar, movie name is removed.
 
 function startApp(){
-    var requestURL = 'http://www.omdbapi.com/?t=barbie&apikey=5ee7c193'; //title, rating, genre, runtime
-    $.ajax({
-        url: requestURL,
-        method: "GET"        
-    })
-        .then(function(response){
-            console.log(response);
-            $('.title').text(response.Title);
-            $('.rating').text("Rated: "+ response.Rated);
-            $('.runtime').text("Runtime: "+ response.Runtime);
-            $('.genre').text("Genre: "+ response.Genre); 
-            var seeList = JSON.parse(localStorage.getItem('seeList'))||[];
-            var title = response.Title;
-            $('.title').on('click', function(){
-                localStorage.setItem('title', title);
-                seeList.unshift(title);
+     var seeList = JSON.parse(localStorage.getItem('seeList'))||[];
+      var requestURLBarbie = 'http://www.omdbapi.com/?t=barbie&apikey=5ee7c193'; //title, rating, genre, runtime
+      $.ajax({
+          url: requestURLBarbie,
+          method: "GET"        
+      })
+          .then(function(response){
+              console.log(response);
+              $('.barbie-title').text(response.Title);
+              $('.barbie-rating').text("Rated: "+ response.Rated);
+              $('.barbie-runtime').text("Runtime: "+ response.Runtime);
+              $('.barbie-genre').text("Genre: "+ response.Genre); 
+              var seeList = JSON.parse(localStorage.getItem('seeList'))||[];
+              var barbieTitle = response.Title;
+              localStorage.setItem('barbie-title', barbieTitle);
+              $('.barbie-title').on('click', function(){
+                seeList.unshift(barbieTitle);
                 localStorage.setItem('seeList',JSON.stringify(seeList));
-                $('.movie-list').append("<li>"+localStorage.getItem('title')+"</li>");
+                  $('.movie-list').append("<li>"+localStorage.getItem('barbie-title')+"</li>");
+              });
+                  var requestURLOpp = 'http://www.omdbapi.com/?t=oppenheimer&apikey=5ee7c193'; //title, rating, genre, runtime
+                  $.ajax({
+                      url: requestURLOpp,
+                      method: "GET"        
+                  })
+                      .then(function(response){
+                          console.log(response);
+                          $('.opp-title').text(response.Title);
+                          $('.opp-rating').text("Rated: "+ response.Rated);
+                          $('.opp-runtime').text("Runtime: "+ response.Runtime);
+                          $('.opp-genre').text("Genre: "+ response.Genre); 
+
+                          var oppTitle = response.Title;
+                          localStorage.setItem('opp-title', oppTitle);
+                          $('.opp-title').on('click', function(){
+                              localStorage.setItem('opp-title', oppTitle);
+                              seeList.unshift(oppTitle);
+                              localStorage.setItem('seeList',JSON.stringify(seeList));
+                              $('.movie-list').append("<li>"+localStorage.getItem('opp-title')+"</li>");
+                  
+                         })
+                  
+               })    
+             })
+
+      
+   }
+      
     
-           })
-    
- })        
-}
+      
+
 
              // 2. This code loads the IFrame Player API code asynchronously.
              var tag = document.createElement('script');
@@ -42,9 +70,10 @@ function startApp(){
        
              // 3. This function creates an <iframe> (and YouTube player)
              //    after the API code downloads.
-             var player;
+             var barbiePlayer;
+             var oppPlayer;
              function onYouTubeIframeAPIReady() {
-               player = new YT.Player('player', {
+               barbiePlayer = new YT.Player('barbie-player', {
                  height: '280',
                  width: '490',
                  videoId: 'pBk4NYhWNMM',
@@ -58,6 +87,20 @@ function startApp(){
 
                  
                });
+               oppPlayer = new YT.Player('opp-player', {
+                height: '280',
+                width: '490',
+                videoId: 'bK6ldnjE3Y0',
+                playerVars: {
+                  'playsinline': 1
+                },
+                events: {
+                  'onReady': onPlayerReady,
+                  'onStateChange': onPlayerStateChange
+                }
+
+                
+              });
              }
        
              // 4. The API will call this function when the video player is ready.
